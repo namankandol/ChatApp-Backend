bab00fb4bc3636040eb382903e5e159a2243fca4
namankandol <82331362+namankandol@users.noreply.github.com>
2024-01-07 19:54:51 +0530

    origin changed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "https://venomsnappy.netlify.app/",
+        origin: process.env.ORIGIN,
     },
 })
 
