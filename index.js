13d19aa4fbed4701b963abff280d721a76fbef01
namankandol <82331362+namankandol@users.noreply.github.com>
2024-01-07 18:39:13 +0530

    first backend commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const userRoutes = require("./routes/userRoutes")
+const messageRoutes = require("./routes/messagesRoutes")
+const socket = require("socket.io")
+
+const app = express()
+require("dotenv").config()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/auth", userRoutes)
+app.use("/api/messages", messageRoutes)
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("DB Connection Successful")
+    })
+    .catch((err) => {
+        console.log(err.message)
+    })
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server has started on port ${process.env.PORT}`)
+})
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
+})
+
+global.onlineUsers = new Map()
+
+io.on("connection", (socket) => {
+    global.chatSocket = socket
+    socket.on("add-user", (userID) => {
+        onlineUsers.set(userID, socket.id)
+    })
+
+    socket.on("send-msg", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to)
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.message)
+        }
+    })
+})
